4362d2a791d64158498ff561fd99f2e2e40bb31b
Lio <liomazza@gmail.com>
2022-07-02 12:17:44 -0300

    game websocket route

--- src/main/kotlin/com/liot/data/routes/WebSocketRoutes.kt
+++ src/main/kotlin/com/liot/data/routes/WebSocketRoutes.kt
@@ -1,16 +1,38 @@
 package com.liot.data.routes
 
 import com.google.gson.JsonParser
+import com.liot.data.Room
 import com.liot.data.models.BaseModel
 import com.liot.data.models.ChatMessage
+import com.liot.data.models.DrawData
 import com.liot.gson
 import com.liot.other.Constants.TYPE_CHAT_MESSAGE
+import com.liot.other.Constants.TYPE_DRAW_DATA
+import com.liot.server
 import com.liot.session.DrawingSession
 import io.ktor.http.cio.websocket.*
 import io.ktor.routing.*
 import io.ktor.sessions.*
 import io.ktor.websocket.*
 import kotlinx.coroutines.channels.consumeEach
+
+fun Route.gameWebSocketRoute() {
+    route("/ws/draw") {
+        standardWebSocket { socket, clientID, message, payload ->
+            when(payload) {
+                is DrawData -> {
+                    val room = server.rooms[payload.roomName] ?: return@standardWebSocket
+                    if(room.phase == Room.Phase.GAME_RUNNING) {
+                        room.broadcastToAllExcept(message, clientID)
+                    }
+                }
+                is ChatMessage -> {
+
+                }
+            }
+        }
+    }
+}
 
 fun Route.standardWebSocket(
     handleFrame: suspend (socket: DefaultWebSocketServerSession,
@@ -32,6 +54,7 @@ fun Route.standardWebSocket(
                    val jsonObject = JsonParser.parseString(message).asJsonObject
                    val type = when(jsonObject.get("type").asString) {
                        TYPE_CHAT_MESSAGE -> ChatMessage::class.java
+                       TYPE_DRAW_DATA -> DrawData::class.java
                        else -> BaseModel::class.java
                    }
                    val payload = gson.fromJson(message, type)
